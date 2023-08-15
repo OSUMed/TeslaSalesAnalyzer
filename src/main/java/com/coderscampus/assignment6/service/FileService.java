e552393e82f1e95b6ba782780c6c3ebdd29ca288
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-14 18:49:34 -0700

    add skeleton for File Service

--- src/main/java/com/coderscampus/assignment6/service/FileService.java
+++ src/main/java/com/coderscampus/assignment6/service/FileService.java
@@ -1,5 +1,28 @@
 package com.coderscampus.assignment6.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.coderscampus.assignment6.domain.Sales;
+
 public class FileService {
+	private Map<String, List<Sales>> salesData;
+	private String filePath;
+
+	public FileService(String filePath) {
+		this.filePath = filePath;
+	}
+
+	private Map<String, List<Sales>> groupData() {
+		return salesData;
+	}
+
+	private void getTitle() {
+
+	}
+
+	private void writeToFile(String title) {
+
+	}
 
 }

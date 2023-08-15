528e3d1cbc13742ee3b31f2a1814b44840b858da
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-14 18:46:02 -0700

    add POGO for sales and CSV files

--- src/main/java/com/coderscampus/assignment6/SalesApplication.java
+++ src/main/java/com/coderscampus/assignment6/SalesApplication.java
@@ -4,8 +4,8 @@ import com.coderscampus.assignment6.service.SalesService;
 
 public class SalesApplication {
 	
-	public static final String[] FILES = { "InterpolWatchReport-Week2.csv", "InterpolWatchReport-Week1.csv", "InterpolWatchReport-Week3.csv" };
-	public static final String[] FILES_TEST = { "InterpolWatchReport-Week1.csv" };
+	public static final String[] FILES = { "model3.csv", "modelS.csv", "modelS.csv" };
+	public static final String[] FILES_TEST = { "modelS.csv" };
 
 	public static void main(String[] args) {
 		for (String file_path: FILES_TEST) {
--- src/main/java/com/coderscampus/assignment6/domain/Sales.java
+++ src/main/java/com/coderscampus/assignment6/domain/Sales.java
@@ -1,5 +1,22 @@
 package com.coderscampus.assignment6.domain;
 
 public class Sales {
-
+	private String date;
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getSales() {
+		return sales;
+	}
+	public void setSales(String sales) {
+		this.sales = sales;
+	}
+	@Override
+	public String toString() {
+		return "Sales [date=" + date + ", sales=" + sales + "]";
+	}
+	private String sales;
 }

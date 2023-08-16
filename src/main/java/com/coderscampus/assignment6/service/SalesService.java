c9e9c24c8396b9861825f37186b853dc9664b27f
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-15 23:17:31 -0700

    Refactor code so more modular

--- src/main/java/com/coderscampus/assignment6/service/SalesService.java
+++ src/main/java/com/coderscampus/assignment6/service/SalesService.java
@@ -30,7 +30,68 @@ public class SalesService {
 
 	public void calculateData() {
 		System.out.println("Print the dict: ");
+
+		// Get the Max and Min:
 		
+		findSum();
+		findMax();
+		findMin();
+		
+		
+		yearMonthMaxFormatted = formatDates(maxMonthYear);
+		yearMonthMinFormatted = formatDates(minMonthYear);
+		
+//		String[] minMonthYearList = minMonthYear.getDate().split("-");
+//		String[] maxMonthYearList = maxMonthYear.getDate().split("-");
+//		DateService dateServiceMin = new DateService(minMonthYearList[0], minMonthYearList[1]);
+//		DateService dateServiceMax = new DateService(maxMonthYearList[0], maxMonthYearList[1]);
+//		yearMonthMaxFormatted = dateServiceMin.getYearMonthString();
+//		yearMonthMinFormatted = dateServiceMax.getYearMonthString();
+
+//		System.out.println(maxMonthYear);
+//		System.out.println(yearMonthMaxFormatted);
+//		System.out.println(minMonthYear);
+//		System.out.println(yearMonthMinFormatted);
+	}
+
+	private void findMax() {
+		maxMonthYear = new Sales("0");
+
+		// Find Max:
+		salesData.entrySet().stream().forEach(entry -> {
+			String key = entry.getKey();
+			List<Sales> salesList = entry.getValue();
+			// Use max to see what element will return -1 against others, and thus, the
+			// greatest
+			Optional<Sales> maxSales = salesList.stream()
+					.max((sales1, sales2) -> sales1.getSales().compareTo(sales2.getSales()));
+			if (maxSales.isPresent()) {
+				if (maxSales.get().getSales().compareTo(maxMonthYear.getSales()) > 0) {
+					maxMonthYear = maxSales.get();
+				}
+			}
+		});
+	}
+
+	private void findMin() {
+		minMonthYear = new Sales(Integer.toString(Integer.MAX_VALUE));
+		// Find Min:
+		salesData.entrySet().stream().forEach(entry -> {
+			String key = entry.getKey();
+			List<Sales> salesList = entry.getValue();
+			// Use max to see what element will return -1 against others, and thus, the
+			// greatest
+			Optional<Sales> minSales = salesList.stream()
+					.min((sales1, sales2) -> sales1.getSales().compareTo(sales2.getSales()));
+			if (minSales.isPresent()) {
+				if (minSales.get().getSales().compareTo(minMonthYear.getSales()) < 0) {
+					minMonthYear = minSales.get();
+				}
+			}
+		});
+	}
+
+	private void findSum() {
 		// Get the Sum:
 		this.salesDataSums = new HashMap<String, BigDecimal>();
 		salesData.entrySet().stream().forEach(entry -> {
@@ -40,91 +101,15 @@ public class SalesService {
 					(accumulator, value) -> accumulator.add(value));
 			this.salesDataSums.put(key, sum);
 		});
-		
-		// Get the Max and Min:
-		maxMonthYear = new Sales("0");
-		minMonthYear = new Sales(Integer.toString(Integer.MAX_VALUE));
-
-		// For Max:
-		salesData.entrySet().stream().forEach(entry -> {
-			String key = entry.getKey();
-			List<Sales> salesList = entry.getValue();
-			// Use max to see what element will return -1 against others, and thus, the greatest
-			Optional<Sales> maxSales = salesList.stream()
-					.max((sales1, sales2) -> sales1.getSales().compareTo(sales2.getSales()));
-			if (maxSales.isPresent()) {	   
-				if (maxSales.get().getSales().compareTo(maxMonthYear.getSales()) > 0) {
-					maxMonthYear = maxSales.get();
-				}
-			}
-		});
-		// For Min:
-		salesData.entrySet().stream().forEach(entry -> {
-			String key = entry.getKey();
-			List<Sales> salesList = entry.getValue();
-		  // Use max to see what element will return -1 against others, and thus, the greatest
-	        Optional<Sales> minSales = salesList.stream()
-	            .min((sales1, sales2) -> sales1.getSales().compareTo(sales2.getSales()));
-	        if (minSales.isPresent()) {	   
-	        	if (minSales.get().getSales().compareTo(minMonthYear.getSales()) < 0) {
-	        		minMonthYear = minSales.get();
-	        	}
-	        }
-		});
-		
-		String[] minMonthYearList = minMonthYear.getDate().split("-");
-		String[] maxMonthYearList = maxMonthYear.getDate().split("-");
-		DateService dateServiceMin = new DateService(minMonthYearList[0], minMonthYearList[1]);
-		DateService dateServiceMax = new DateService(maxMonthYearList[0], maxMonthYearList[1]);
-		yearMonthMaxFormatted = dateServiceMin.getYearMonthString();
-		yearMonthMinFormatted = dateServiceMax.getYearMonthString();
-
-		System.out.println(yearMonthMaxFormatted);
-		System.out.println(yearMonthMinFormatted);
-//		for (Map.Entry<String, BigDecimal> entry2 : this.salesDataSums.entrySet()) {
-//			String key2 = entry2.getKey();
-//			BigDecimal value2 = entry2.getValue();
-//			System.out.println(key2 + " total is: " + value2);
-//		}
-//        for key in salesData:
-//            yearMonthMax = findMax(value)
-//            yearMonthMin = findMin(value)
-//            3 times so try updating yearMonthMax/yearMonthMin
-//		  
-//		  Get the formatted dates for exporting:
-//		  yearMonthMaxFormatted = getDate(yearMonthMax)
-//		  yearMonthMinFormatted = getDate(yearMonthMin)
-
-//		  Go through each year and sum them and insert into gra:
-//        for key in salesData:
-//            sum = findSum(value)
-//		  	  salesDataSums[key] = sum
 	}
-//
-//	private void findMax() {
-//
-////        Stream -> max
-////        return month & year
-//	}
-//
-//	private void findMin() {
-//
-////    	Stream -> min
-////    	return month & year
-//	}
-//
-//	private void findSum() {
-//
-////    	Stream -> update salesDataSums
-//	}
-//
-//	private void formatDates(String yearMonth) {
-//
-////		year, month = yearMonth
-////				dateService = new DateService(month, year)
-////				dateService.createFormattedDate
-//	}
-//
+
+	private String formatDates(Sales yearMonth) {
+		String[] monthYearList = yearMonth.getDate().split("-");
+		DateService dateService = new DateService(monthYearList[0], monthYearList[1]);
+		String yearMonthFormatted = dateService.getYearMonthString();
+		return yearMonthFormatted;
+	}
+
 //	private void writeToFile() {
 ////        fileService.writeToFile(pass title, formatted max/min, salesDataSums)
 }
